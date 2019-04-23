70b3551559b900e1c74648ed5117a16d11d5d5f8
Cameron Sapp <chs170030@utdallas.edu>
2019-04-22 22:10:13 -0500

    base game logic added to ClientController

--- src/model/GoFishGame.java
+++ src/model/GoFishGame.java
@@ -1,19 +1,33 @@
 package model;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.net.Socket;
+
 import java.util.Iterator;
 
 public class GoFishGame extends CardGame
 {
+	public GoFishGame(int numOfPlayers, ArrayList<String> playerNames, ArrayList<Socket> clientSocks,
+		File cardList) {
+		super(numOfPlayers, playerNames, clientSocks, cardList);
+	}
+	
 	public boolean isLegalMove(Player focusPlayer, String move) {
 		// TODO
+		return true;
 	}
 	
 	public boolean queryPlayer(Card card, Player source, Player target) {
 		// TODO
+		return false;
 	}
 	
 	public String determineWinner(PlayerQueue playerList) {
 		Iterator<Player> playerIter = playerList.iterator();
 		// TODO
+		return null;
 	}
 }
--- src/model/GoFishQueue.java
+++ src/model/GoFishQueue.java
@@ -3,12 +3,4 @@ package model;
 public class GoFishQueue extends PlayerQueue
 {
 	public Player playAgain() {return getPlayer();}
-	
-	public Player dequeue(Player person) {
-		
-	}
-	
-	private void dequeue(Node n) {
-		
-	}
 }
